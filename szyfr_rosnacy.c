9eceda6ea23832a7d4afdb3cb2b98291898d4423
Krzysztof Rokicki <krzysrok@gmail.com>
2021-01-15 11:36:34 +0100

    Stworzenie funkcji szyfrujacej tekst w szyfrze rosnacym

---
+++ szyfr_rosnacy.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int wartosc = 0;
+    char znak;
+    znak = getc(stdin);
+    while(true){
+
+        //Sprawdzamy czy nie powrocilismy do pierwotnej litery
+        if(wartosc==27)
+            wartosc = 1;
+
+        //Sprawdzamy czy nasz znak jest duza litera
+        if(znak >= 65 && znak <= 90)
+        {
+            //Sprawdzamy czy zaszyfrowany znak bedzie litera
+            if(znak + wartosc > 90)
+                znak = znak + wartosc - 26;
+            else
+                znak = znak + wartosc;
+        }
+
+        //Sprawdzamy czy nasz znak jest mala litera
+        if(znak >= 97 && znak <= 122)
+        {
+            //Sprawdzamy czy zaszyfrowany znak bedzie litera
+            if(znak + wartosc > 122)
+                znak = znak + wartosc - 26;
+            else
+                znak = znak + wartosc;
+        }
+
+        wartosc++;
+        printf("%c",znak);
+        znak = getc(stdin);
+
+        // Zamyka program po wpisaniu znaku "/"
+        if(znak==47){
+            return 0;
+        }
+    }
+}
