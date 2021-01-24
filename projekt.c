5cf330f0c6b48748b2945253d0898a56ac70916f
Yana <yanavashkevich4@gmail.com>
2021-01-24 10:56:43 +0300

    teraz dziala na wejsciu wiekszym niz 50

--- projekt.c
+++ projekt.c
@@ -1,50 +1,45 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-//Szyfrowanie
-#define N 50
-int main()
+int main( )
 {
-    char text[N];
-    FILE *plik;
-    plik=fopen("tekst.txt","r");
-    FILE *wyjscie;
-    wyjscie=fopen("wynik.txt","w");
+FILE *plik;
+plik=fopen("tekst.txt","r");
+FILE *wyjscie;
+wyjscie=fopen("wynik.txt","w");
 
-    fgets(text, N, plik);
-    fprintf(stdout, "%s\n", text);
+char text;
 
-    char result[N];
+if ((plik = fopen("tekst.txt","r")) == NULL)
+{
+printf("Error opening file ");
+exit(1); /* wyjście z programu w 
+przypadku błędu otwarcia pliku tekst.txt*/
+}
+
+while(feof(plik)==0)
+{
+text = fgetc(plik); 
+    
+char result;
     int k1,k2;
     k1=3;
     k2=5; 
     
-   
-    int i,z=0;
-    int l = strlen(text); 
-   // długość tekstu 
 
-    for(i=0;i<l;i++)
-    {
-        if (text[i] >= 'A' && text[i] <= 'Z'){
-            result[z]=(k1*(text[i]-'A')+k2)%26+'A';
-        } else if (text[i] >= 'a' && text[i] <= 'z'){
-            result[z]=(k1*(text[i]-'a')+k2)%26+'a';
+        if (text >= 'A' && text <= 'Z'){
+            result=(k1*(text-'A')+k2)%26+'A';
+        } else if (text >= 'a' && text <= 'z'){
+            result=(k1*(text-'a')+k2)%26+'a';
         } else{
-            result[z] = text[i];
+            result = text;
         }
-        z++;
-    }
-    int m=0;
-    while(m<l){
-    fprintf(wyjscie,"%c", result[m]);
-    m++;}
+    
 
-    fclose(plik);
-    fclose(wyjscie);
+     fprintf(wyjscie,"%c",result);
+     
+}
 
-
-    return 0;
-
-   }
-
+fclose(plik);
+fclose(wyjscie);
+}
